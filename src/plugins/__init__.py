08c1b26791c64f6d4f6d0b9a5a507740a0dd4d19
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-15 14:55:50 +0530

    Update __init__.py

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -3,4 +3,4 @@ import logging as _logging
 _logger = _logging.getLogger(__name__)
 
 def setup(*args, **kwds):
-    _logger.info("Succesfully loaded plugins")
+    _logger.error("Succesfully loaded plugins")
