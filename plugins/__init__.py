aae6207ca1eb06d0ae27cb89f90f3d1475d4609f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-06-16 09:00:03 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,14 +1,14 @@
 from pymongo import AsyncMongoClient
 from plugins.modules import handlers
 
-def setup(
-    app: "Yukkibot",  # https://github.com/TheTeamVivek/YukkiMusic/blob/master/YukkiMusic/corebot.py
-    userbot: "Userbot",  # https://github.com/TheTeamVivek/YukkiMusic/blob/master/YukkiMusic/core/call.py
-    call: "Call",  # https://github.com/TheTeamVivek/YukkiMusic/blob/master/YukkiMusic/core/call.py
-    mongo_client : AsyncMongoClient, # https://github.com/TheTeamVivek/YukkiMusic/blob/master/YukkiMusic/core/mongo.py
-    config,  # https://github.com/TheTeamVivek/YukkiMusic/tree/master/config
-    helpable: dict,  # https://github.com/TheTeamVivek/YukkiMusic/blob/master/YukkiMusic/__init__.py#L38 HELPABLE
-):
+def setup(ctx: "LoaderContext"):
+    """
+    ctx: LoaderContext instance provided by the main application.
+
+    For details, see the LoaderContext class in YukkiMusic core modules:
+    https://github.com/TheTeamVivek/YukkiMusic/tree/master/YukkiMusic/core/modules.py
+    """
+
     handlers, help = handlers()
     for h, group in handlers:
       app.add_handler(h, group)
